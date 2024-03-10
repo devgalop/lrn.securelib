2775a67e15274e859fe0f15662f7c07ceee0d999
devgalop <devgalop@gmail.com>
2024-03-10 14:43:12 -0500

    Add WebApi configuration
    Implements bases to run the app in dev environment

--- lrn.devgalop.securelib.Webapi/Program.cs
+++ lrn.devgalop.securelib.Webapi/Program.cs
@@ -1,7 +1,14 @@
+using lrn.devgalop.securelib.Infrastructure.Security.JWT.Extensions;
+using lrn.devgalop.securelib.Infrastructure.Security.EncryptDecrypt.Extensions;
+using lrn.devgalop.securelib.Infrastructure.Security.JWT.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddJwtSecurity();
+builder.Services.AddAesEncryption();
+builder.Services.AddRSAEncryption();
+builder.Services.AddHealthChecks();
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,30 +22,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseMiddleware<JwtAuthenticationMiddelware>();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
+app.MapHealthChecks("/healthy");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
