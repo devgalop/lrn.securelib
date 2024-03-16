42e7d4cbe5eb9ff0d11f7186183ee400ed0b7f63
devgalop <devgalop@gmail.com>
2024-03-16 05:54:50 -0500

    Fix TokenResponse model name
    Change the word Response insted of Reponse

--- lrn.devgalop.securelib.Infrastructure/Security/JWT/Models/TokenResponse.cs
+++ lrn.devgalop.securelib.Infrastructure/Security/JWT/Models/TokenResponse.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace lrn.devgalop.securelib.Infrastructure.Security.JWT.Models
 {
-    public class TokenReponse : BaseResponse
+    public class TokenResponse : BaseResponse
     {
         public string Token { get; set; } = string.Empty;
         public DateTime Expiration { get; set; }
