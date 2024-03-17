ac3018665e0a1f798adce8db3e2c2ed50fd5ef23
devgalop <devgalop@gmail.com>
2024-03-17 16:31:10 -0500

    Fix issue totp dependency injection
    Add totpParameters injection

--- lrn.devgalop.securelib.Infrastructure/Security/TOTP/Extensions/TotpExtensions.cs
+++ lrn.devgalop.securelib.Infrastructure/Security/TOTP/Extensions/TotpExtensions.cs
@@ -15,7 +15,13 @@ namespace lrn.devgalop.securelib.Infrastructure.Security.TOTP.Extensions
         public static void AddTOTP(this IServiceCollection services)
         {
             EpochTicks epochTicks = new();
+            TotpParameters totpParameters = new()
+            {
+                Size = 6,
+                ValidForTimeSpan = TimeSpan.FromSeconds(30)  
+            };
             services.AddSingleton(_ => epochTicks);
+            services.AddTransient(_ => totpParameters);
             services.AddAesEncryption();
             services.AddTransient<ITotpFactoryService,TotpFactoryService>();
         }
